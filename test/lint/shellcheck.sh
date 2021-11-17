ece1a2e3a4b02d0bf1ade801bc698cfe2604ad19
Naomi Rose <23469+norosa@users.noreply.github.com>
2021-11-17 10:31:37 +0000

    Add all-permissive license to supporting fies

--- test/lint/shellcheck.sh
+++ test/lint/shellcheck.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -e
 
+# Copyright 2021, Naomi Rose and contributors
+#
+# Copying and distribution of this file, with or without modification, are
+# permitted in any medium without royalty, provided the copyright notice and
+# this notice are preserved. This file is offered as-is, without any warranty.
+
 # ANSI codes
 DIM=[2m
 RESET=[0m
