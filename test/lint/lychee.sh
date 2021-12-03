750bccacc8837584869b3d3f14b3a68dff96f81d
Naomi Rose <23469+norosa@users.noreply.github.com>
2021-12-03 09:53:58 +0000

    WIP

---
+++ test/lint/lychee.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -e
+
+lint_out="${LINT_OUT:=}"
+
+if test -z "${lint_out}"; then
+    echo "Error: \`LINT_OUT\` not set"
+    exit 1
+fi
+
+# Find elibible files in the current directory, skipping the largest
+# directories that would be ignored by Git
+find_files() {
+    # Preemptively ignore the largest directories that would be ignored by Git
+    find . -type "f" ! -path "./.git/*" ! -path "./.venv/*" | sort |
+        while read -r file; do
+            # Skip any files ignored by Git
+            if git check-ignore "${file}" >/dev/null; then
+                continue 2
+            fi
+            if file -b --mime-encoding "${file}" | grep -q "binary"; then
+                continue 2
+            fi
+            echo "${file}" | sed -E 's/^\.\///'
+        done
+}
+
+run_lychee() {
+    output="${1}"
+    # This program is relatively slow, so we run it in parallel
+    # TODO: Perhaps the name of `parallel-moreutils` configurable?
+    find_files |
+        xargs parallel-moreutils test/wrappers/lychee.sh -- 2>"${output}"
+}
+
+# Using a pipe means the exit value of the first command will be ignored,
+# allowing `softener` to determine which severity level should result in an
+# error
+output="$(mktemp)"
+run_lychee "${output}"
+softener parse --quiet --write "${lint_out}" \
+    --error-on="notice" --parser="lychee" --format="txt" - <"${output}"
+rm -f "${output}"
---
+++ test/wrappers/prettier.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+# This script wraps the command to produce a more useful output (i.e., line
+# numbers and an error message)
+
+file="${1}"
+
+# This program is relatively slow, so we make an exception and print progress
+# information
+echo "Checking: ${file}"
+
+output="$(mktemp)"
+
+prettier --no-color --ignore-unknown "${file}" >"${output}" || true
+
+if test -s "${output}"; then
+    diff "${file}" "${output}" |
+        grep -E "^[0-9]" |
+        sed 's/c.*//' | sed 's/,/:/' |
+        while read -r line; do
+            echo "${file}:${line}:Incorrect formatting" >&2
+        done
+fi
+
+rm -rf "${output}"
