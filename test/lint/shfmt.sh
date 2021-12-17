7b43c0b715e1a00a8eadffbab96947c725aa7f29
Restyled.io <commits@restyled.io>
2021-12-17 22:03:14 +0000

    Restyled by shellharden

--- test/lint/shfmt.sh
+++ test/lint/shfmt.sh
@@ -2,7 +2,7 @@
 
 lint_out="${LINT_OUT:=}"
 
-if test -z "${lint_out}"; then
+if test -z "$lint_out"; then
     echo "Error: \`LINT_OUT\` not set"
     exit 1
 fi
@@ -14,20 +14,20 @@ find_files() {
     find . -type "f" ! -path "./.git/*" ! -path "./.venv/*" |
         while read -r file; do
             # Skip any files ignored by Git
-            if git check-ignore "${file}" >/dev/null; then
+            if git check-ignore "$file" >/dev/null; then
                 continue 2
             fi
-            mime_type="$(file --mime-type --brief "${file}")"
-            case "${mime_type}" in
+            mime_type="$(file --mime-type --brief "$file")"
+            case "$mime_type" in
             text/x-shellscript)
                 # Include all shell scripts
-                echo "${file}"
+                echo "$file"
                 ;;
             text/plain)
                 # Include files with a `shellcheck` directive on the first line
-                if head -n 1 "${file}" |
+                if head -n 1 "$file" |
                     grep "# shellcheck shell" >/dev/null; then
-                    echo "${file}"
+                    echo "$file"
                 fi
                 ;;
             # Default NOOP
@@ -41,16 +41,16 @@ run_shfmt() {
     # seperated by newlines, avoiding the problem of needing NULL bytes to
     # avoid problems with filenames containing spaces
     tmp_file="$(mktemp)"
-    find_files >"${tmp_file}"
+    find_files >"$tmp_file"
     while read -r file; do
-        test/wrappers/shfmt.sh "${file}"
-    done <"${tmp_file}"
-    rm "${tmp_file}"
+        test/wrappers/shfmt.sh "$file"
+    done <"$tmp_file"
+    rm "$tmp_file"
 }
 
 # Using a pipe means the exit value of the first command will be ignored,
 # allowing `obelist` to determine which severity level should result in an
 # error
 run_shfmt |
-    obelist parse --quiet --console --write "${lint_out}" \
+    obelist parse --quiet --console --write "$lint_out" \
         --error-on="notice" --parser="shfmt" --format="txt" -
--- test/wrappers/shfmt.sh
+++ test/wrappers/shfmt.sh
@@ -7,15 +7,15 @@ file="${1}"
 
 output="$(mktemp)"
 
-shfmt -p -i 4 "${file}" >"${output}" || true
+shfmt -p -i 4 "$file" >"$output" || true
 
-if test -s "${output}"; then
-    diff "${file}" "${output}" |
+if test -s "$output"; then
+    diff "$file" "$output" |
         grep -E "^[0-9]" |
         sed 's/c.*//' | sed 's/,/:/' |
         while read -r line; do
-            echo "${file}:${line}:Incorrect formatting"
+            echo "$file:$line:Incorrect formatting"
         done
 fi
 
-rm -rf "${output}"
+rm -rf "$output"
