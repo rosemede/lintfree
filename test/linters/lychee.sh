e35427fbeea874e1353dc0d5cf74f30edb195fe9
Naomi <23469+norosa@users.noreply.github.com>
2021-12-20 00:37:00 +0000

    Add `lychee` integration
    To get this working properly, I had to pull out the shell script
    responsible for matching URLs to line numbers into a Python script. This
    Python script is now used by both the `markdown-link-check` and `lychee`
    wrappers.

--- test/linters/lychee.sh
+++ test/linters/lychee.sh
@@ -2,20 +2,18 @@
 
 . test/lib/setup/linter.sh
 
-run_markdown_link_check() {
+run_lychee() {
     # This program is relatively slow, so we run it in parallel
     test/lib/tools/find-all-files.sh |
-        grep -E '\.md$' |
-        xargs parallel-moreutils test/wrappers/markdown-link-check.sh --
-
+        grep -vE '\.md$' |
+        xargs parallel-moreutils test/wrappers/lychee.sh --
 }
 
 # Using a pipe means the exit value of the first command will be ignored,
 # allowing `obelist` to determine which severity level should result in an
 # error
 output="$(mktemp)"
-run_markdown_link_check 2>"${output}"
+run_lychee 2>"${output}"
 obelist parse --quiet --console --write "${lint_out}" \
-    --error-on="notice" --parser="markdown-link-check" --format="txt" - \
-    <"${output}"
+    --error-on="notice" --parser="lychee" --format="txt" - <"${output}"
 rm -f "${output}"
